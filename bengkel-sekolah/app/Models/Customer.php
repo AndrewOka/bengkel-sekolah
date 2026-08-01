04107e2ee6be7905aa94f5505d2f605a8e8ac30b
andrew <andrewchristianoka04@gmail.com>
2026-08-01 23:45:06 +0800

    finish

--- bengkel-sekolah/app/Models/Customer.php
+++ bengkel-sekolah/app/Models/Customer.php
@@ -4,19 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes; // 1. Import SoftDeletes
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Customer extends Model
 {
-    use HasFactory, SoftDeletes; // 2. Tambahkan SoftDeletes di sini
+    use HasFactory, SoftDeletes;
 
-    protected $table = 'customers';
-    protected $primaryKey = 'customer_id'; // Sesuaikan jika primaryKey kamu menggunakan customer_id atau id
+    protected $primaryKey = 'customer_id';
 
-    protected $fillable = [
-        'customer_code',
-        'full_name',
-        'phone',
-        'type',
-    ];
+    protected $guarded = [];
 }
